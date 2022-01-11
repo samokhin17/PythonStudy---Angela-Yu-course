e99f144e80bb69d230bbab15dd614dfc33fa3e2f
samokhin17 <88451055+samokhin17@users.noreply.github.com>
2022-01-11 14:26:42 +0300

    09 еще раз классы, объекты, методы, конструкторы

--- Training/Geometry.py
+++ Training/Geometry.py
@@ -2,14 +2,14 @@ from math import sqrt
 
 
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, _x, _y):
+        self.x = _x
+        self.y = _y
 
     def distance(self, p2):
         dx = p2.x - self.x
         dy = p2.y - self.y
-        return sqrt(dx*dx + dy*dy)
+        return sqrt(dx * dx + dy * dy)
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
