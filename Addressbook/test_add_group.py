05f8a0eaf7f5c5d615c8f24b1904347d362c4f6b
samokhin17 <88451055+samokhin17@users.noreply.github.com>
2022-01-12 17:20:38 +0300

    2.1 Перенес навигацию в логин и криейт групп

--- Addressbook/test_add_group.py
+++ Addressbook/test_add_group.py
@@ -17,20 +17,14 @@ class UntitledTestCase(unittest.TestCase):
     
     def test_add_group(self):
         driver = self.driver
-        self.open_home_page(driver)
         self.login(driver, username="admin", password="secret")
-        self.open_groups_page(driver)
         self.create_group(driver, Group(name="777", header="777", footer="777"))
-        self.return_to_groups_page(driver)
         self.logout(driver)
 
     def test_add_empty_group(self):
         driver = self.driver
-        self.open_home_page(driver)
         self.login(driver, username="admin", password="secret")
-        self.open_groups_page(driver)
         self.create_group(driver, Group(name="", header="", footer=""))
-        self.return_to_groups_page(driver)
         self.logout(driver)
 
     def logout(self, driver):
@@ -40,6 +34,7 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_link_text("group page").click()
 
     def create_group(self, driver, group):
+        self.open_groups_page(driver)
         # init group creation
         driver.find_element_by_name("new").click()
         # fill group form
@@ -54,11 +49,13 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         driver.find_element_by_name("submit").click()
+        self.return_to_groups_page(driver)
 
     def open_groups_page(self, driver):
         driver.find_element_by_link_text("groups").click()
 
     def login(self, driver, username, password):
+        self.open_home_page(driver)
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys(username)
