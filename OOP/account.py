5da4f67237d63e3340bd53e0f90044e8bcecfe9f
samokhin17 <88451055+samokhin17@users.noreply.github.com>
2022-05-12 13:30:53 +0300

    OOP

--- OOP/account.py
+++ OOP/account.py
@@ -1,14 +1,30 @@
-
+import pytz
+import datetime
 
 class Account:
     def __init__(self, name, balance):
+        self.history = []
         self.balance = balance
         self.name = name
 
+
     def deposit(self, amount):
         self.balance += amount
+        self.show_balance()
+        self.history.append([amount, pytz.utc.localize(datetime.utcnow())])
 
     def withdraw(self, amount):
         if self.balance > amount:
             self.balance -= amount
+            print(f'You spent {amount} units')
+            self.show_balance()
+        else:
+            print('Not enough money')
+            self.show_balance()
+
+    def show_balance(self):
+        print(f'Balance: {self.balance}')
+
+a = Account('Sasha', 0)
+
 
